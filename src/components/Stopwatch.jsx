30997aa7e47d491bdd4ca71e74ecfd787c38109e
Jordan Devaney <jordandevaney@Jordans-MacBook-Pro.local>
2020-03-29 22:28:12 -0400

    build logic for stopwatch

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,13 +1,32 @@
-import React , { useState } from 'react';
+import React , { useState, useEffect } from 'react';
 
 const Stopwatch = props => {
-    const [ timer, setTimer ] = useState(0);
+    const [ seconds, setSeconds ] = useState(0);
+    const [ isRunning, setIsRunning ] = useState(false);
+
+    useEffect(() => {
+        let interval = null;
+        if(isRunning) {
+            interval = setInterval(() => setSeconds(seconds => seconds + 1), 1000);
+        } else if (!isRunning && seconds !== 0) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [isRunning, seconds])
+
+    const handleStopwatch = () => {
+        setIsRunning(!isRunning);
+    }
+    const handleReset = () => {
+        setSeconds(0);
+        setIsRunning(false);
+    }
     return (
         <div className="stopwatch">
             <h2>Stopwatch</h2>
-            <span className="stopwatch-time">0</span>
-            <button>Start</button> 
-            <button>Reset</button>
+            <span className="stopwatch-time">{ seconds }</span>
+            <button onClick={handleStopwatch}>{isRunning ? 'Stop' :  'Start'}</button> 
+            <button onClick={handleReset}>Reset</button>
         </div>
     )
 }
